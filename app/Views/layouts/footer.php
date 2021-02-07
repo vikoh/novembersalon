04a4584a8a55801f214e93ad9c718af58e426ef7
vijay <vthapa@nksquare.co.in>
2021-02-07 16:29:16 +0530

    some changes done in header and footer; added favicon

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -1,9 +1,5 @@
-<em>&copy; 2019</em>
 </body>
-
-<!-- MDB -->
 <script type="text/javascript" src="<?= base_url('assets/js/mdb.min.js') ?>"></script>
 <script type="text/javascript" src="<?= base_url('assets/js/script.js') ?>"></script>
-<!-- Custom scripts -->
 <script type="text/javascript"></script>
 </html>
--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -1,18 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <title><?= $title ?></title>
-    <!-- MDB icon -->
-    <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon" />
-    <!-- Font Awesome -->
+    <meta name="description" content="November Salon - A beauty parlour in Kohima. Services includes hair straightening, hair coloring, facial, hair spa etc.">
+    <link rel="icon" href="<?= base_url('assets/img/favicon.ico') ?>" type="image/x-icon" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
-    <!-- Google Fonts Roboto -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
-    <!-- MDB -->
     <link rel="stylesheet" href=" <?= base_url('assets/css/mdb.min.css') ?>" />
     <link rel="stylesheet" href=" <?= base_url('assets/css/style.css') ?>" />
 </head>
+
 <body>
