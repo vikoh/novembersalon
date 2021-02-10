8cd155440f6585c13a59724a93e729d549ed1fab
vijay <vthapa@nksquare.co.in>
2021-02-10 14:23:17 +0530

    home page

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -2,7 +2,7 @@
 <div class="container">
     <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
         <div class="text-center">
-            <img class="mb-4" src="<?= base_url('assets/img/november-salon-logos.jpeg') ?>" style="width: 500px; height: 500px;" />
+            <img class="mb-4 img-fluid" src="<?= base_url('assets/img/november-salon-logos.jpeg') ?>" style="width: 500px; height: 500px;" />
             <h5 class="mb-3 text-white">Content for November Salon coming soon!</h5>
         </div>
     </div>
